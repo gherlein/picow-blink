5de32a259f8a3582ab23c0067fe915243daeb1b6
Greg Herlein <gherlein@herlein.com>
2024-07-11 15:27:23 -0700

    working

--- main.c
+++ main.c
@@ -1,29 +1,24 @@
-/* 
-    This code originates from the Getting started with Raspberry Pi Pico document
-    https://datasheets.raspberrypi.org/pico/getting-started-with-pico.pdf
-    CC BY-ND Raspberry Pi (Trading) Ltd
-*/
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
-#include <stdio.h>
+#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
-#include "hardware/gpio.h"
-#include "pico/binary_info.h"
-
-const uint LED_PIN = 25;
 
 int main() {
-    bi_decl(bi_program_description("PROJECT DESCRIPTION"));
-    
-    stdio_init_all();
-
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    while(1) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
-        sleep_ms(1000);
-    }
+  stdio_init_all();
+  if (cyw43_arch_init()) {
+    printf("Wi-Fi init failed");
+    return -1;
+  }
+  while (true) {
+    printf("on\n");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(250);
+    printf("off\n");
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(250);
+  }
 }
